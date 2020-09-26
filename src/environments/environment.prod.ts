2202d80026cba6e032fb3d4dc3a3efff54d7a621
Jamie Introcaso <jamie.introcaso@gmail.com>
2020-09-26 10:17:10 -0400

    update for environment prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: true,
+  COSMIC_BUCKET: '',
+  COSMIC_READ_KEY: '',
+  COSMIC_WRITE_KEY: '',
   COSMIC_API_URL: 'https://api.cosmicjs.com/v1/',
+  COSMIC_CAMPAIGN_NAME: '',
   runtimeConfigUrl: 'https://heuristic-minsky-ea8416.netlify.app/.netlify/functions/get-environment-variables'
 };
